21571d8d4e3ec85e5c0396955a45b5236a0f7390
gvndalai <gundalai254@gmail.com>
2023-12-06 12:20:50 +0800

    dec6 little heaven

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,16 +13,41 @@ export default function Home() {
             <div>Work</div>
             <div>Testimonials</div>
             <div>Contact</div>
+            <span></span>
           </div>
-          <hr></hr>
           <div className="nav-bar-right-seperated-right">
             <img src="icon.png"></img>
             <button id="download-cv-button">Download CV</button>
           </div>
         </div>
       </div>
-      <h1>Hi, I'm Gundalai</h1>
-      <p>I'm a developer course's student at Pinecone academy</p>
+      <div className="general-intro">
+        <div className="intro">
+          <h1>Erwin Smith ⚔️</h1>
+          <p>
+            Erwin Smith (エルヴィン・スミス Eruvin Sumisu?) was the 13th
+            commander (団長 Danchō?) of the Scout Regiment. Discerning,
+            intelligent, and widely respected, Erwin was an able commander.
+            While he cared deeply for his men, he did not hesitate to sacrifice
+            them for the good and prosperity of mankind, and his men have proven
+            more than willing to stake their lives at his order. Erwin developed
+            the Long-Distance Enemy Scouting Formation. Due to uncertainty about
+            his future, he later named Hange Zoë as his successor.`
+          </p>
+          <div className="conditon">
+            <div className="location">
+              <img src="location.png"></img>
+              Paradis island
+            </div>
+            <div className="job-status">
+              <img src="greendot.png"></img>
+              Too busy dying for humanity.
+            </div>
+          </div>
+          <img src="Links.png" id="links"></img>
+        </div>
+        <img src="erwin.png"></img>
+      </div>
     </main>
   );
 }
