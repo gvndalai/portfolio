75bcf073be19bbd79e14f6cc72cce9c9ca79f6c3
gvndalai <gundalai254@gmail.com>
2023-12-12 15:50:54 +0800

    dec12ajildeer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { LightModeIcon } from "./Icons/LightModeIcon";
 import { SSicon } from "./Icons/SSIcon";
 import { DarkModeIcon } from "./Icons/DarkModeIcon";
+import { Menu } from "./Menu";
 
 export const Header = () => {
   return (
-    <div class=" w-full flex justify-between px-[80px] `sm:p-[16px]` py-[16px]  ">
+    <div class="w-full flex justify-between sm:px-20 p-4 sm:py-4 relative">
       <SSicon />
-      <div class="lg:flex sm:flex lg:gap-6 hidden lg:block ">
+      <div class=" absolute p-[22px] right-0 top-0 block sm:hidden">
+        {" "}
+        <Menu />
+      </div>
+      <div class="sm:flex sm:gap-6 hidden sm:block">
         <div class="flex gap-6 items-center">
           <div class="hover:text-gray-900 cursor-pointer text-gray-600">
             About
@@ -23,6 +28,7 @@ export const Header = () => {
           </div>
           <span class="w-0 h-6 border border-solid border-gray-300"></span>
         </div>
+
         <div class="flex gap-4 items-center relative">
           <LightModeIcon />
           <DarkModeIcon />
--- src/Components/JobStatusTitle.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export const JobStatusTitle = () => {
-  return <h1> {JobStatusTitle.title}</h1>;
-};
