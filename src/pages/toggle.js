b0d599f0fca7f68f3d73871997451f4b5ba69301
Gundalai Dawgasuren <23LP0094@G304-CC0042.local>
2023-12-18 19:28:58 +0800

    dec18 Pinecone

--- src/pages/toggle.js
+++ src/pages/toggle.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Toggle = () => {
-  const [color, setColor] = useState(false);
-  const [text, setText] = useState(false);
-  function colorChange() {
-    setColor(!color);
-    setText(!text);
-  }
+const Counter = () => {
+  const [minutes, setMinute] = useState(0);
+  const addMinutes = () => {
+    setMinute(minutes + 1);
+  };
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     addMinute();
+  //   }, 60);
+  //   return () => clearInterval(interval);
+  // });
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+    addMinutes();
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className={`${color ? "bg-red-600" : "bg-green-400"}`}>
-      {/* <OnButton bgcolor={color} change={colorChange} /> */}
-      {/* <div> */}
-      <button onClick={colorChange}>{`${text ? "Off" : "On"}`}</button>
-      {/* </div> */}
+    <div className="flex justify-center items-center gap-2">
+      {/* {!!minute ? <div>{minute}</div> : <p>minute -iin utga 0 baina</p>} */}
+      <div>
+        {minutes}:{seconds}
+      </div>
+
+      {/* <button onClick={add}>add</button> */}
+      {/* <button onClick={() => setIsVisible(!isVisible)}>stop</button> */}
     </div>
   );
 };
-export default Toggle;
+export default Counter;
