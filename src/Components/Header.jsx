7cbb0375476400c894c3e6f1491071ed9a4d4be4
gvndalai <gundalai254@gmail.com>
2024-01-06 19:09:51 +0800

    download cv

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { LightModeIcon } from "./icons/LightModeIcon";
 import { SSicon } from "./icons/SSIcon";
 import { DarkModeIcon } from "./icons/DarkModeIcon";
 import { Menu } from "./Menu";
-
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 export const Header = () => {
+  const downloadPdf = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("img/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(
+        componentWidth / imgWidth,
+        componentHeight / imgHeight
+      );
+      const imgX = (componentWidth - imgWidth * ratio) / 2;
+      const imgY = 30;
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        componentWidth * ratio,
+        componentHeight * ratio
+      );
+      pdf.save("invoice.pdf");
+    });
+  };
+  const pdfRef = useRef();
   return (
     <div class="w-full flex justify-between sm:px-20 p-4 sm:py-4 relative">
       <SSicon />
@@ -32,7 +60,10 @@ export const Header = () => {
         <div class="flex gap-4 items-center relative">
           <LightModeIcon />
           <DarkModeIcon />
-          <button class="bg-gray-900 text-gray-100 w-34 h-9 text-base font-medium px-4 py-1 m-0 rounded-xl hover:bg-gray-700 transition active:bg-gray-800">
+          <button
+            class="bg-gray-900 text-gray-100 w-34 h-9 text-base font-medium px-4 py-1 m-0 rounded-xl hover:bg-gray-700 transition active:bg-gray-800"
+            onClick={downloadPdf()}
+          >
             Download CV
           </button>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,16 @@ import { Page1 } from "@/components/page1/Page1";
 import { Work } from "@/components/Work";
 import { Footer } from "@/components/Footer";
 import { Timer } from "@/components/Timer";
+import { useRef } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 const IndexPage = () => {
+  const pdfRef = useRef();
   return (
     <div
       className="mx-auto sm:w-[1440px] w-full flex flex-col items-center"
       id="background"
+      ref={pdfRef}
     >
       <Timer />
       <Header />
